3aa0227037315f3285cb6326ff46ac874a06ec60
tralucent <tralucent@gmail.com>
2022-01-22 11:12:00 -0800

    Initial Commit

---
+++ Shared/chronologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chronologyApp.swift
+//  Shared
+//
+//  Created by Halcyone Rapp on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct chronologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
